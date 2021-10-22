548ed18466ab515c9589088137ece351b8092ac7
Maximilian Wagner <maw3350@gmail.com>
2021-10-21 21:22:19 -0400

    Add text to model_b.rb

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+vehicle = {
+  "make" => 'Toyota',
+  "model" => 'Yaris',
+  "maximum_mph" => 120,
+  "maximum_mpg" => 44,
+  "is_fuel_efficient" => true,
+  "is_cool" => false,
+  "available_colors" => ['green', 'blue', 'red'],
+  "dealers_in_stock" => ['Stafford', 'Fredericksburg', 'Hartwood', 'Fairfax'],
+}
