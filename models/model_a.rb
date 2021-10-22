dcd773a6d0fc0a215739ec3bb59963bcaa5da92f
Maximilian Wagner <maw3350@gmail.com>
2021-10-21 21:13:16 -0400

    Add text to model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+airport = {
+  "name" => 'Bigtime Airport',
+  "nearest_town" => 'Denver, CO',
+  "number_of_planes" => 10578,
+  "parking_lot_capacity" => 50,
+  "is_open" => true,
+  "is_haunted" => true,
+  "pilot_names" => ['Ben', 'Bob', 'Bo'],
+  "pilot_ages" => [101, 104, 130]
+}
