68d5c801338fc6270e0466489cfac4ff197d7679
Maximilian Wagner <maw3350@gmail.com>
2021-10-21 20:58:37 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "Bill Gates"
+is_active = true
+famous_person = "Steve Jobs"
+num_3 = 1
+num_9 = 2
+sum = num_3 + num_9
+animals = ['pig', 'moose', 'bird']
+arr.shift
+puts famous_person.count
